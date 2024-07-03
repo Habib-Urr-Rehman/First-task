c56bb12b0b5a44fb3843ff9c4e32f4d1d52851e6
Habib-Urr-Rehman <habib585712@gmail.com>
2024-07-03 14:27:54 +0500

    Updated forms.py and using get_object_or_404

--- address_book/contacts/forms.py
+++ address_book/contacts/forms.py
@@ -13,11 +13,11 @@ class ContactForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
 
 def clean_phone_number(self):
-    phone_number = self.cleaned_data['phone_number']
+    phone_numbers = self.cleaned_data['phone_number']
     try:
-        contact = get_object_or_404(Contact, phone_number=phone_number)
+        contact = get_object_or_404(Contact, phone_number=phone_numbers)
         if self.instance and self.instance == contact:
-            return phone_number
+            return phone_numbers
         raise forms.ValidationError("This phone number already exists.")
     except Contact.DoesNotExist:
-        return phone_number
+        return phone_numbers

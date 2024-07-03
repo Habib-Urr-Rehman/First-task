63d942135cc8e6d4d5bce399a582dd82020d2e63
Habib-Urr-Rehman <habib585712@gmail.com>
2024-07-03 14:21:38 +0500

    Updated forms.py and using get_object_or_404

--- address_book/contacts/forms.py
+++ address_book/contacts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.validators import RegexValidator
 from contacts.models import Contact
+from django.shortcuts import get_object_or_404
 
 class ContactForm(forms.ModelForm):
     phone_number = forms.CharField(max_length=15, validators=[
@@ -11,16 +12,12 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
 
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data['phone_number']
-        try:
-            # Check if any contact with this phone number exists
-            contacts = Contact.objects.filter(phone_number=phone_number)
-            if contacts.exists():
-                # If editing an existing contact, allow the same phone number
-                if self.instance and self.instance.pk in contacts.values_list('pk', flat=True):
-                    return phone_number
-                raise forms.ValidationError("This phone number already exists.")
+def clean_phone_number(self):
+    phone_number = self.cleaned_data['phone_number']
+    try:
+        contact = get_object_or_404(Contact, phone_number=phone_number)
+        if self.instance and self.instance == contact:
             return phone_number
-        except Contact.DoesNotExist:
-            return phone_number
+        raise forms.ValidationError("This phone number already exists.")
+    except Contact.DoesNotExist:
+        return phone_number

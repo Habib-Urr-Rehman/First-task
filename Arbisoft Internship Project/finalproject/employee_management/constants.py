f476bf4a7a3f8decb6ff2ac8a54738a409c05428
Habib-Urr-Rehman <habib585712@gmail.com>
2024-08-06 22:07:12 +0500

    Complete Project Modele

---
+++ Arbisoft Internship Project/finalproject/employee_management/constants.py
@@ -0,0 +1,63 @@
+# Choice Constants
+TYPE_CHOICES = (
+    ('IT Services', 'IT Services'),
+    ('Software Development', 'Software Development'),
+    ('Hardware Manufacturing', 'Hardware Manufacturing'),
+    ('Networking', 'Networking'),
+    ('Cybersecurity', 'Cybersecurity'),
+    ('Cloud Computing', 'Cloud Computing'),
+    ('Mobiles Phones','Mobiles phones'),
+)
+
+POSITION_CHOICES = (
+    ('Manager', 'Manager'),
+    ('Software Developer', 'Software Developer'),
+    ('Project Leader', 'Project Leader'),
+    ('Product Manager', 'Product Manager'),
+    ('Quality Assurance Engineer', 'Quality Assurance Engineer'),
+    ('Frontend Developer', 'Frontend Developer'),
+    ('Backend Developer', 'Backend Developer'),
+    ('Full Stack Developer', 'Full Stack Developer'),
+    ('Security Engineer', 'Security Engineer'),
+)
+
+REIMBURSEMENT_TYPE_CHOICES = (
+    ('Medical', 'Medical'),
+    ('Internet', 'Internet'),
+    ('Travel', 'Travel'),
+    ('Parking', 'Parking'),
+    ('Education', 'Education'),
+    ('Fitness', 'Fitness'),
+    ('Training', 'Training'),
+)
+
+LEAVE_TYPE_CHOICES = (
+    ('Sick', 'Sick'),
+    ('Casual', 'Casual'),
+    ('Earned', 'Earned'),
+    ('Annual', 'Annual'),
+    ('Unpaid', 'Unpaid'),
+)
+
+TASK_CHOICES = (
+    ('Coding', 'Coding'),
+    ('Training', 'Training'),
+    ('Tutorial', 'Tutorial'),
+    ('Meeting', 'Meeting'),
+    ('Check', 'Check'),
+    ('Code Review', 'Code Review'),
+    ('Team Engagement', 'Team Engagement'),
+    ('Documentation', 'Documentation'),
+    ('Research', 'Research'),
+    ('Design', 'Design'),
+    ('Testing', 'Testing'),
+    ('Debugging', 'Debugging'),
+    ('Planning', 'Planning'),
+    ('Client Communication', 'Client Communication'),
+)
+
+ROLE_CHOICES = (
+    ('admin', 'Admin'),
+    ('employee', 'Employee'),
+)
+
